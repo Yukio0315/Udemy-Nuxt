0bf4baa67ddfeaad54d7cc5445c8f04cf742bb32
Yukio Ueda <52945670+Yukio0315@users.noreply.github.com>
2020-01-01 12:46:26 +0900

    completed first lecture

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,21 +1,9 @@
 <template>
-  <div>
-    <input v-model="userId" type="text" />
-    <button @click="onLoadUser">Load User</button>
-  </div>
+  <p>Please enter user id</p>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      userId: ''
-    }
-  },
-  methods: {
-    onLoadUser() {
-      this.$router.push('/users/' + this.userId)
-    }
-  }
+  layout: 'users'
 }
 </script>
